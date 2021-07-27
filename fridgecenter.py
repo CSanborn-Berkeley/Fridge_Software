a3ae764c3f574602236dd8b43a44a16e6d58778f
CSanborn-Berkeley <88007993+CSanborn-Berkeley@users.noreply.github.com>
2021-07-27 14:34:14 -0700

    Updated File Organization
    Fridgecenter should be the main file that gets called. UI elements should host all kivy functions and classes. Currently have a very barebones ui with the three major tabs put together

---
+++ fridgecenter.py
@@ -0,0 +1,6 @@
+###################################################################
+# Main piece of code, should be as lightweight as possible        #
+# All Kivy declarations should be in UIelements.py if possible    #
+# All functions should be in functions.py                         #
+# Ideally this file will just call the main UI and do little else #
+###################################################################
