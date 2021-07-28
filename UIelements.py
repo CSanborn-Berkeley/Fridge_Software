6bf7e4dc7d543f63385aad30ee829f2ee8f8cb91
CSanborn-Berkeley <88007993+CSanborn-Berkeley@users.noreply.github.com>
2021-07-28 12:05:14 -0700

    Laying out UI Elements
    Tabs are 100% laid out. Elements within tabs are approximately 50% laid out. There is an issue where when adding measurement tabs, all even numbered tabs move the focus to the plus tab, but all odd numbered tabs grab focus as intended. Need to use kivy-garden to integrate matplotlib graphs with this application.

--- UIelements.py
+++ UIelements.py
@@ -3,6 +3,7 @@
 # Most functions of the UI will also end up here          #
 ###########################################################
 
+
 from kivy.lang import Builder
 
 from kivymd.app import MDApp
@@ -11,67 +12,38 @@ from kivymd.font_definitions import fonts
 from kivymd.uix.tab import MDTabsBase
 from kivymd.icon_definitions import md_icons
 
-KV = '''
-MDBoxLayout:
-    orientation: "vertical"
-
-
-    MDTabs:
-        id: tabs
-        on_ref_press: app.on_ref_press(*args)
-
-
-<Tab>
-
-    MDIconButton:
-        id: icon
-        icon: app.icons[0]
-        user_font_size: "48sp"
-        pos_hint: {"center_x": .5, "center_y": .5}
-'''
-
-major_tab_keys = ['monitor','graph','file-settings']
-major_tab_names = ['Monitor', 'Measure','Settings']
-
-
 class Tab(MDFloatLayout, MDTabsBase):
     '''Class implementing content for a tab.'''
+    def build(self):
+        print("New Tab")
 
 
 class FridgeCenter(MDApp):
     icons = list(md_icons.keys())[15:30]
+    MeasurementN = 1
 
     def build(self):
-        return Builder.load_string(KV)
+        self.theme_cls.primary_palette = "Blue"
+        self.theme_cls.primary_hue = '800'
+        return Builder.load_file("FridgeCenter.kv")
 
-    def on_start(self):
-        for name_tab, title_tab in zip(major_tab_keys,major_tab_names):
-            self.root.ids.tabs.add_widget(Tab(icon = name_tab,title= title_tab))
+    def on_measurement_tab_switch(self,MeasurementTabs,TabName,LabelObj,Ref):
+        if TabName.icon == "plus":
+            name_tab = "Measurement "+ str(self.MeasurementN)
+            tab_text = f"{name_tab} [ref={name_tab}][font={fonts[-1]['fn_regular']}]{md_icons['close']}[/font][/ref]"
+            NewTab = Tab(text=tab_text)
+            MeasurementTabs.add_widget(NewTab)
+            MeasurementTabs.switch_tab(tab_text)
+            self.MeasurementN+=1
 
-    def on_ref_press(
-        self,
-        instance_tabs,
-        instance_tab_label,
-        instance_tab,
-        instance_tab_bar,
-        instance_carousel,
-    ):
-        '''
-        The method will be called when the ``on_ref_press`` event
-        occurs when you, for example, use markup text for tabs.
 
-        :param instance_tabs: <kivymd.uix.tab.MDTabs object>
-        :param instance_tab_label: <kivymd.uix.tab.MDTabsLabel object>
-        :param instance_tab: <__main__.Tab object>
-        :param instance_tab_bar: <kivymd.uix.tab.MDTabsBar object>
-        :param instance_carousel: <kivymd.uix.tab.MDTabsCarousel object>
-        '''
+    def on_measurement_ref_press(self,MeasurementTabs,TabsLabel,MainTab,TabsBar,TabsCarousel):
+        print("Ref Press")
+        for instance_tab in TabsCarousel.slides:
+            if instance_tab.text == TabsLabel.text:
+                MeasurementTabs.remove_widget(TabsLabel)
 
-        # Removes a tab by clicking on the close icon on the left.
-        for instance_tab in instance_carousel.slides:
-            if instance_tab.text == instance_tab_label.text:
-                instance_tabs.remove_widget(instance_tab_label)
-                break
+
 
 
 FridgeCenter().run()
